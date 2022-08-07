d66550c0edaffffbc80877cc14cb5cdf3245a8de
Luki446 <luki446@gmail.com>
2022-08-07 18:12:23 +0200

    #4 Add basic login frontend

--- Docely/ClientApp/src/app/login/login.component.ts
+++ Docely/ClientApp/src/app/login/login.component.ts
@@ -1,4 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { environment } from '../../environments/environment';
+
+
+interface UserResult{
+  name: string;
+  surName: string;
+  email: string;
+  password: string;
+}
+
+interface LoginResult {
+  user: UserResult;
+  token: string;
+  refreshToken: string;
+  authDate: Date;
+  succeeded: boolean;
+  statusCode: number;
+  errors: string[];
+}
 
 @Component({
   selector: 'app-login',
@@ -6,9 +27,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public formBuilder: FormBuilder,
+    public http: HttpClient
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: ['']
+    });
+  }
 
+  login() {
+    const postData = {
+      login: 'BORYSIAK',
+      password: 'string'
+    };
+
+    this.http.post<LoginResult>(environment.apiUrl + '/Authenticate/Login', postData).subscribe(
+      (data: LoginResult) => {
+        console.log(data);
+      }
+    );
+  }
   ngOnInit(): void {
   }
 
